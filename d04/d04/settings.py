75e6a4a3b59e6f7e0cef1c67f2df358b2b5b220f
Marina-28 <greatundeadmoose@gmail.com>
2023-02-26 23:07:12 +0300

    ex03

--- d04/d04/settings.py
+++ d04/d04/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ex00.apps.Ex00Config',
     'ex01.apps.Ex01Config',
-    'ex02.apps.Ex02Config'
+    'ex02.apps.Ex02Config',
+    'ex03.apps.Ex03Config'
 ]
 
 MIDDLEWARE = [
